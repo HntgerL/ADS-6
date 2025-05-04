32650223ff6fc70938a9db820d59e90f72f1d2f9
HntgerL <lbedevd@gmail.com>
2025-05-04 21:36:27 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -11,44 +11,53 @@ class TPQueue {
     T data;
     explicit Node(const T& data) : data(data), next(nullptr) {}
   };
-  Node* head;
+   Node* head;
 
  public:
   TPQueue() : head(nullptr) {}
-  ~TPQueue() {
-    while (head != nullptr) {
-      Node* temp = head;
-      head = head->next;
-      delete temp;
-      }
+  ~TPQueue();
+  void push(const T& el);
+  bool isEmpty() const { return head == nullptr; }
+  T pop();
+};
+
+template<typename T>
+TPQueue<T>::~TPQueue() {
+  while (head != nullptr) {
+    Node* temp = head;
+    head = head->next;
+    delete temp;
   }
-  void push(const T& el) {
-    Node* nNode = new Node(el);
-    if (el.prior > head->data.prior || head == nullptr) {
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+  if (isEmpty())
+    throw std::runtime_error("Queue is empty");
+  Node* temp = head;
+  T data = temp->data;
+  head = head->next;
+  delete temp;
+  return data;
+}
+
+template<typename T>
+void TPQueue<T>::push(const T& el) {
+  Node* nNode = new Node(el);
+    if (head == nullptr || el.prior > head->data.prior) {
       nNode->next = head;
       head = nNode;
     } else {
       Node* curr = head;
       Node* prev = nullptr;
-      while (curr != nullptr && curr->data.prior >= el.prior) {
+      while (curr != nullptr && curr->data.prior >= elem.prior) {
         prev = curr;
         curr = curr->next;
       }
       prev->next = nNode;
       nNode->next = curr;
-    }
   }
-  bool isEmpty() const { return head == nullptr; }
-  T pop() {
-    if (isEmpty())
-      throw std::runtime_error("queue is empty");
-    Node* temp = head;
-    T data = temp->data;
-    head = head->next;
-    delete temp;
-    return data;
-  }
-};
+}
 
 struct SYM {
   char ch;
